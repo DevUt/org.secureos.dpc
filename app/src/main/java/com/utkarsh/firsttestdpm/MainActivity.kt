db96fa2bac6ac26bc0c8ff48d36d138d93f772a8
Utkarsh Tiwari <DevUt@users.noreply.github.com>
2022-01-20 21:26:47 +0530

    Test diabling of packages

--- app/src/main/java/com/utkarsh/firsttestdpm/MainActivity.kt
+++ app/src/main/java/com/utkarsh/firsttestdpm/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.utkarsh.firsttestdpm
 
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 import android.widget.TextView
+import com.utkarsh.firsttestdpm.DeviceAdmin.device_admin
 
 const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cn = ComponentName(this,device_admin::class.java)
+        val dpm = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        val disableSwitch : Switch = findViewById(R.id.disable_switch)
+
+        disableSwitch.setOnClickListener{
+            if(disableSwitch.isChecked){
+                dpm.setApplicationHidden(cn,"com.whatsapp",true)
+            }else{
+                dpm.setApplicationHidden(cn,"com.whatsapp",false)
+            }
+        }
     }
 }
