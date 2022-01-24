e3995827089e5c501418aa3758942c5739f9b83a
Utkarsh Tiwari <DevUt@users.noreply.github.com>
2022-01-24 10:55:21 +0530

    Make a non-functional password policy

--- app/src/main/java/org/secureos/dpc/deviceAdmin/DeviceAdmin.kt
+++ app/src/main/java/org/secureos/dpc/deviceAdmin/DeviceAdmin.kt
@@ -29,13 +29,19 @@ class DeviceAdmin : DeviceAdminReceiver(){
         dpm = context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
         cn = ComponentName(context.applicationContext,DeviceAdmin::class.java)
         Log.d(TAG,"Starting to enforce Policy")
-        Log.d(TAG,"Enabling package policy")
+        Log.d(TAG,"Starting to enforce Password policy")
+        enablePasswordPolicy(context.applicationContext)
+        Log.d(TAG,"Success in setting password policy")
+        Log.d(TAG,"Starting to enforce Package policy")
         enablePackagePolicy(context.applicationContext)
-        Log.d(TAG,"Enabled package policy")
-        Log.d(TAG,"Enabling permission policy")
+        Log.d(TAG,"Enforced package policy")
+        Log.d(TAG,"Starting to enforce Permission policy")
         // Wait on permission Policy
         enablePermissionPolicy(context.applicationContext)
-        Log.d(TAG,"Enabled permission policy")
+        Log.d(TAG,"Enforced permission policy")
+        Log.d(TAG,"Starting to enforce camera Policy")
+        enableCameraPolicy(context.applicationContext)
+        Log.d(TAG,"Enforced Camera Policy")
     }
     private fun enablePackagePolicy(context: Context){
         Log.d(TAG,"Enabling Package Policy")
@@ -63,8 +69,8 @@ class DeviceAdmin : DeviceAdminReceiver(){
             }
         }
     }
-    private fun enableMiscPolicy(context: Context){
-        Log.d(TAG,"Enabling Misc Page")
+    private fun enableCameraPolicy(context: Context){
+        Log.d(TAG,"Enabling Camera Policy")
         val miscPref = MiscPrefManager(context)
         runBlocking {
             if(miscPref.readEnabled("camera") == 1){
@@ -73,5 +79,20 @@ class DeviceAdmin : DeviceAdminReceiver(){
         }
     }
 
+    private fun enablePasswordPolicy(context: Context){
+        Log.d(TAG,"Checking compliance to Password standards")
+        dpm.requiredPasswordComplexity = DevicePolicyManager.PASSWORD_COMPLEXITY_HIGH
+        while(!dpm.isActivePasswordSufficientForDeviceRequirement){
+            Toast.makeText(context,"Set a better password",Toast.LENGTH_LONG).show()
+            val passIntent = Intent(context,DevicePolicyManager.ACTION_SET_NEW_PASSWORD::class.java)
+            context.applicationContext.startActivity(passIntent)
+        }
+        val maxWipeTries = runBlocking {
+            MiscPrefManager(context.applicationContext).readEnabled("wipe_retries")
+        }
+        Log.d(TAG,"Setting Maximum Failed Password Limit")
+        dpm.setMaximumFailedPasswordsForWipe(cn,maxWipeTries?:0)
+        Log.d(TAG,"Successfully Set Maximum Failed Password Limit")
+    }
 
 }
--- app/src/main/java/org/secureos/dpc/misc/Misc.kt
+++ app/src/main/java/org/secureos/dpc/misc/Misc.kt
@@ -3,6 +3,8 @@ package org.secureos.dpc.misc
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import kotlinx.coroutines.runBlocking
 import org.secureos.dpc.R
 import java.security.AccessControlContext
@@ -13,14 +15,34 @@ class Misc : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_misc)
         val cameraSwitch = findViewById<com.google.android.material.switchmaterial.SwitchMaterial>(R.id.camera_switch)
+        var currCameraStatus : Int?
+        runBlocking {
+            currCameraStatus = MiscPrefManager(this@Misc).readEnabled("camera")
+        }
+        if(currCameraStatus != null){
+            cameraSwitch.isChecked = (currCameraStatus == 1)
+        }
         cameraSwitch.setOnClickListener {
             runBlocking {
                 val writeCameraD = MiscPrefManager(this@Misc)
-                writeCameraD.writeEnabled("camera",(cameraSwitch.isChecked==false).toInt())
+                writeCameraD.writeEnabled("camera",(!cameraSwitch.isChecked).toInt())
+            }
+        }
+
+        val saveButton : Button = findViewById(R.id.misc_save)
+        val minWipeTries : com.google.android.material.textfield.TextInputEditText = findViewById(R.id.password_wipe_length)
+        saveButton.setOnClickListener {
+            if(minWipeTries.text.toString().isNullOrBlank()){
+                Toast.makeText(this,"Text Field Can't be empty",Toast.LENGTH_LONG).show()
+                recreate()
+            }else{
+                runBlocking {
+                    MiscPrefManager(this@Misc).writeEnabled("wipe_retries",minWipeTries.text.toString().toInt())
+                }
             }
         }
 
     }
-    fun Boolean.toInt() = if (this) 1 else 0
+    private fun Boolean.toInt() = if (this) 1 else 0
 
 }
